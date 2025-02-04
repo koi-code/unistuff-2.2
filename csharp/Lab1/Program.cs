c2fc4221abb262893bddcaefddddb38e32f43f0a
znimator <zmeicreative@gmail.com>
2025-02-04 22:10:27 +0300

    CSharp/Lab1

---
+++ csharp/Lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите символ: ");
+        
+string? input = Console.ReadLine();
+
+char symbol = !string.IsNullOrEmpty(input) ? input[0] : 'a';
+
+Console.Write("Введите высоту пирамиды: ");
+int height = int.Parse(Console.ReadLine() ?? "1");
+
+for (int row = 1; row <= height; row++)
+{
+    
+    for (int space = 0; space < height - row; space++)
+    {
+        Console.Write(' ');
+    }
+    
+    for (int x = 0; x < 2 * row - 1; x++)
+    {
+        Console.Write(symbol);
+    }
+    Console.WriteLine();
+}
